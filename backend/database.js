789a0383f1a8ac3f75bc3876a0701cf043fc808f
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-05-12 19:15:54 +0530

    first commit

---
+++ backend/database.js
@@ -0,0 +1,30 @@
+const users = [
+  {
+    email: "theo@gmail.com",
+    password: "123123",
+  },
+  {
+    email: "john@gmail.com",
+    password: "asdasd",
+  },
+];
+
+const publicPosts = [
+  {
+    title: "Post 1",
+    content: "Post 1 content",
+  },
+  {
+    title: "Post 2",
+    content: "Post 2 content",
+  },
+];
+
+const privatePosts = [
+  {
+    title: "Private Post",
+    content: "Private Post",
+  },
+];
+
+module.exports = { users, publicPosts, privatePosts };
---
+++ backend/routes/auth.js
@@ -0,0 +1,178 @@
+const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
+const { users } = require("../database");
+const { sign } = require("jsonwebtoken");
+
+require("dotenv").config();
+
+router.post(
+  "/signup",
+  [
+    check("email", "Invalid email").isEmail(),
+    check("password", "Password must be 6 char long").isLength({
+      min: 6,
+    }),
+  ],
+
+  async (req, res) => {
+    const { email, password } = req.body;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    let user = users.find((user) => {
+      return user.email === email;
+    });
+
+    if (user) {
+      return res.status(200).json({
+        errors: [
+          {
+            email: user.email,
+            msg: "The User already exists",
+          },
+        ],
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    console.log("salt:", salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log("hashed password:", hashedPassword);
+
+    users.push({
+      email,
+      password: hashedPassword,
+    });
+
+    const accessToken = await JWT.sign(
+      { email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "10s",
+      }
+    );
+    res.json({
+      accessToken,
+    });
+  }
+);
+
+router.get("/users", (req, res) => {
+  res.json(users);
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  let user = users.find((user) => {
+    return user.email === email;
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      errors: [
+        {
+          msg: "Invalid credentials",
+        },
+      ],
+    });
+  }
+
+  let isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(401).json({
+      errors: [
+        {
+          msg: "Email or password is invalid",
+        },
+      ],
+    });
+  }
+
+  const accessToken = await JWT.sign(
+    { email },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "10s",
+    }
+  );
+
+  const refreshToken = await JWT.sign(
+    { email },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "1m",
+    }
+  );
+
+  refreshTokens.push(refreshToken);
+
+  res.json({
+    accessToken,
+    refreshToken,
+  });
+});
+
+let refreshTokens = [];
+
+router.post("/token", async (req, res) => {
+  const refreshToken = req.header("x-auth-token");
+
+  if (!refreshToken) {
+    res.status(401).json({
+      errors: [
+        {
+          msg: "Token not found",
+        },
+      ],
+    });
+  }
+
+  if (!refreshTokens.includes(refreshToken)) {
+    res.status(403).json({
+      errors: [
+        {
+          msg: "Invalid Refresh Token",
+        },
+      ],
+    });
+  }
+
+  try {
+    const user = await JWT.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    const { email } = user;
+    const accessToken = await JWT.sign(
+      { email },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "10s" }
+    );
+    res.json({ accessToken });
+  } catch (error) {
+    res.status(403).json({
+      errors: [
+        {
+          msg: "Invalid token",
+        },
+      ],
+    });
+  }
+});
+
+
+router.delete("/logout", (req, res) => {
+    const refreshToken = req.header("x-auth-token");
+    refreshTokens = refreshTokens.filter((token)=> token !== refreshToken);
+    res.sendStatus(204);
+})
+
+module.exports = router;
